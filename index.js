50867e86d3491e0b682451b266cd5737a33a6dc6
Kayla Rosario <rosariok314@gmail.com>
2022-08-03 18:44:30 -0400

    added my connection to index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express')
 const mysql = require('mysql')
+// my connection
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: ''
+})
+
+// 
