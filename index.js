8cceddbcabdda160144bf58ffcaa52e512b7ec41
Kayla Rosario <rosariok314@gmail.com>
2022-08-03 22:42:09 -0400

    show all departments and show all roles in index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const mysql = require('mysql')
+const inquirer = require('inquirer');
+const consoleTable = require('console.table')
+const mysql = require('mysql2');
+
+
 // my connection
 const db = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
-    password: ''
-})
+    password: '',
+    database: 'nodedbsql'
+});
+
+
 
 // connection to mysql
 db.connect(err => {
@@ -13,18 +19,86 @@ db.connect(err => {
         throw err 
     }
     console.log('MySQL Connection')
-})
+});
 
-// connecting to express
-const app = express()
-
-// my database
-app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE nodedbsql'
-    db.query(sql, err => {
-        if(err) {
-            throw err 
+sqlConnect = () => {
+    console.log("___________________________________")
+    console.log("|               ***               |")
+    console.log("|        EMPLOYEE TRACKER         |")
+    console.log("|               ***               |")
+    console.log("----------------------------------")
+    promptUser();
+  };
+  
+  // inquirer prompt for first action
+  const promptUser = () => {}
+    inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'choices', 
+        message: 'What would you like to do?',
+        choices: ['View departments',
+                  'Add department', 
+                  'View employees',
+                  'Add employee',
+                  'View roles',
+                  'Add role',
+                  'exit']
+      }
+    ])
+      .then((answers) => {
+        const { choices } = answers; 
+  
+        if (choices === "View departments") {
+          showDepartments();
         }
-        res.send('Database Created')
+  
+        if (choices === "View roles") {
+          showRoles();
+        }
+  
+        if (choices === "View employees") {
+          showEmployees();
+        }
+  
+        if (choices === "Add department") {
+          addDepartment();
+        }
+  
+        if (choices === "Add role") {
+          addRole();
+        }
+  
+        if (choices === "Add employee") {
+          addEmployee();
+        }
+  
+        if (choices === "exit") {
+          updateEmployee();
+        }});
+// to show all the departments
+viewDepartments = () => {
+  console.log('all departments...\n');
+  const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  });
+};
+
+// to show all the roles of the employee
+viewRoles = () => {
+    console.log('all roles...\n');
+  
+    const sql = `SELECT role.id, role.title, department.name AS department
+                 FROM role
+                 INNER JOIN department ON role.department_id = department.id`;
+    
+    connection.promise().query(sql, (err, rows) => {
+      if (err) throw err; 
+      console.table(rows); 
+      promptUser();
     })
-})
+  };
