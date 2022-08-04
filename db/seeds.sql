3e1104ea1bc57a172a4d727bc24de695a0032a7a
Kayla Rosario <rosariok314@gmail.com>
2022-08-03 20:20:58 -0400

    added employee with first last name role and manager

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,11 +8,23 @@ VALUES
 
 INSERT INTO role (title, salary, departmentID)
 VALUES
-('Accountant', 130000, 2),
-('Account Manager', 98000, 2),
-('Sales Lead', 80000, 4),
-('Salesperson', 120000, 4),
-('Lead Engineer',160000, 1),
-('Software Engineer', 150000, 1),
-('Legal Team', 90000, 3),
-('Lawyer',200000, 3);
+('Accountant', 130000, 1),
+('Account Manager', 98000, 1),
+('Sales Lead', 80000, 2),
+('Salesperson', 120000, 2),
+('Lead Engineer',160000, 3),
+('Software Engineer', 150000, 3),
+('Legal Team', 90000, 4),
+('Lawyer',200000, 4);
+/* not sure if tagged right ^ */
+
+INSERT INTO employee (firstName, lastName, roleID, managerID)
+VALUES 
+('Jane', 'Doe', 1, ''),
+('Colson', 'Baker', 1, 1),
+('Machela', 'Rivera', 2, ''),
+('Jayda', 'Linn', 2, 2),
+('Michelle', 'Castro', 3, ''),
+('John', 'Doe', 3, 3),
+('Jackson', 'Avery', 4, ''),
+('Kevin', 'Rogers', 4, 4);
