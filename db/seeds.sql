78a7a41ecb18c561dbedaeb1a9060aa725fd36c9
Kayla Rosario <rosariok314@gmail.com>
2022-08-03 20:07:36 -0400

    added departments into seeds and added roles in seeds as well

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,18 @@
 /* including wanted commands */
 INSERT INTO department (departmentName)
 VALUES
-('')
+('Engineer'),
+('Finance'),
+('Legal'),
+('Sales');
+
+INSERT INTO role (title, salary, departmentID)
+VALUES
+('Accountant', 130000, 2),
+('Account Manager', 98000, 2),
+('Sales Lead', 80000, 4),
+('Salesperson', 120000, 4),
+('Lead Engineer',160000, 1),
+('Software Engineer', 150000, 1),
+('Legal Team', 90000, 3),
+('Lawyer',200000, 3);
